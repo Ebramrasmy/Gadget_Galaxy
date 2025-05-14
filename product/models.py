2101c0dd8207133638d6cf0369ffbb4b5677f5de
Ebramrasmy <ebramrasmy7@gmail.com>
2025-05-14 23:03:26 +0300

    commit

--- product/models.py
+++ product/models.py
@@ -7,7 +7,8 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    image = models.ImageField(upload_to='products/imgs', blank=True, null=True)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
     sku = models.CharField(max_length=100, unique=True)
     date_added = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
--- product/views.py
+++ product/views.py
@@ -9,12 +9,13 @@ def product_list(request):
     products = Product.objects.all()
     return render(request, 'product/product_list.html', {'products': products})
 
+
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('add_product')
+            return redirect('product_list')
     else:
         form = ProductForm()
     return render(request, 'product/add_product.html', {'form': form})
