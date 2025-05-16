ce2b7d3ca6694113bfc2413b5942c3e9f2cf4a0a
Ebramrasmy <ebramrasmy7@gmail.com>
2025-05-16 22:16:13 +0300

    edit2

--- product/models.py
+++ product/models.py
@@ -1,14 +1,66 @@
 from django.db import models
 from category.models import Category
+from django.shortcuts import redirect
 
-# Create your models here.
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
-    image = models.ImageField(upload_to='products/images', blank=True, null=True)
     sku = models.CharField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='products/images', blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     is_deleted = models.BooleanField(default=False)
+
+    @classmethod
+    def getall(cls):
+        return cls.objects.filter(is_deleted=False)
+
+    @classmethod
+    def get_by_id(cls, id):
+        return cls.objects.get(pk=id)
+
+    @classmethod
+    def Add(cls, name, description, price, stock, sku, image, category):
+        return cls.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            stock=stock,
+            sku=sku,
+            image=image,
+            category=category
+        )
+
+    @classmethod
+    def update(cls, id, name, description, price, stock, sku, image=None, category=None):
+        product = cls.objects.get(pk=id)
+        product.name = name
+        product.description = description
+        product.price = price
+        product.stock = stock
+        product.sku = sku
+        if image:
+            product.image = image
+        if category:
+            product.category = category
+        product.save()
+
+    @classmethod
+    def softdelete(cls, id):
+        cls.objects.filter(pk=id).update(is_deleted=True)
+
+    @classmethod
+    def harddel(cls, id):
+        cls.objects.filter(pk=id).delete()
+
+    @staticmethod
+    def go_to_product_list():
+        return redirect('product:product_list')
+
+    def __str__(self):
+        return self.name
+
+
--- product/views.py
+++ product/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render, redirect ,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product, Category
 from django.core.exceptions import ValidationError
 
 def product_list(request):
-    products = Product.objects.filter(is_deleted=False)
+    products = Product.getall()
     return render(request, 'product/product_list.html', {'products': products})
 
 def product_new(request):
     categories = Category.objects.all()
+    errormsg = None
 
     if request.method == 'POST':
         name = request.POST.get('name')
@@ -17,8 +18,6 @@ def product_new(request):
         sku = request.POST.get('sku')
         image = request.FILES.get('image')
         category_id = request.POST.get('category')
-
-        errormsg = None
 
         if not all([name, description, price, stock, sku, category_id]):
             errormsg = "Please fill all required fields."
@@ -32,7 +31,7 @@ def product_new(request):
             if not errormsg:
                 try:
                     category = Category.objects.get(id=category_id)
-                    product = Product(
+                    product = Product.Add(
                         name=name,
                         description=description,
                         price=price,
@@ -41,17 +40,13 @@ def product_new(request):
                         image=image,
                         category=category
                     )
-                    product.full_clean()
-                    product.save()
-                    return redirect('product:product_list')
+                    return Product.go_to_product_list()
                 except Category.DoesNotExist:
                     errormsg = "Selected category does not exist."
                 except ValidationError as e:
                     errormsg = e.messages
 
-        return render(request, 'product/new.html', {'errormsg': errormsg, 'categories': categories})
-
-    return render(request, 'product/new.html', {'categories': categories})
+    return render(request, 'product/new.html', {'categories': categories, 'errormsg': errormsg})
 
 def product_update(request, product_id):
     product = get_object_or_404(Product, id=product_id)
@@ -79,19 +74,17 @@ def product_update(request, product_id):
             if not errormsg:
                 try:
                     category = Category.objects.get(id=category_id)
-                    product.name = name
-                    product.description = description
-                    product.price = price
-                    product.stock = stock
-                    product.sku = sku
-                    product.category = category
-
-                    if image:
-                        product.image = image
-
-                    product.full_clean()
-                    product.save()
-                    return redirect('product:product_list')
+                    Product.update(
+                        id=product_id,
+                        name=name,
+                        description=description,
+                        price=price,
+                        stock=stock,
+                        sku=sku,
+                        image=image,
+                        category=category
+                    )
+                    return Product.go_to_product_list()
                 except Category.DoesNotExist:
                     errormsg = "Selected category does not exist."
                 except ValidationError as e:
@@ -106,23 +99,19 @@ def product_update(request, product_id):
     return render(request, 'product/new.html', context)
 
 def product_soft_delete(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    product.is_deleted = True
-    product.save()
-    return redirect('product:product_list')
+    Product.softdelete(product_id)
+    return Product.go_to_product_list()
 
 def product_hard_delete(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    product.delete()
-    return redirect('product:product_list')
+    Product.harddel(product_id)
+    return Product.go_to_product_list()
 
 def deleted_products(request):
     deleted = Product.objects.filter(is_deleted=True)
     return render(request, 'product/deleted_list.html', {'products': deleted})
 
-
 def product_restore(request, pk):
     product = get_object_or_404(Product, pk=pk, is_deleted=True)
     product.is_deleted = False
     product.save()
-    return redirect('product:deleted_products')
+    return Product.go_to_product_list()
