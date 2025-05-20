3c21744fd38de84e71f2e64e869d8aba329c993c
Ebramrasmy <ebramrasmy7@gmail.com>
2025-05-20 14:10:01 +0300

    insert , list , updet , delet by Generic Views

--- category/urls.py
+++ category/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
+from .views import *
 
 app_name = 'category'
 
 urlpatterns = [
-    path('', views.category_list, name='category_list'),
-    path('new/', views.category_new, name='category_new'),
-    path('<int:category_id>/edit/', views.category_update, name='category_update'),
-    path('<int:category_id>/delete/', views.category_soft_delete, name='category_soft_delete'),
-    path('<int:category_id>/hard_delete/', views.category_hard_delete, name='category_hard_delete'),
-    path('deleted/', views.deleted_categories, name='deleted_categories'),
-    path('<int:category_id>/restore/', views.category_restore, name='category_restore'),
+    path('', CategoryListView.as_view(), name='category_list'),
+    path('new/', CategoryCreateView.as_view(), name='category_new'),
+    path('update/<int:pk>/', CategoryUpdateView.as_view(), name='category_update'),
+    path('delete/<int:pk>/', CategoryDeleteView.as_view(), name='category_delete'),
+    # path('<int:category_id>/hard_delete/', views.category_hard_delete, name='category_hard_delete'),
+    # path('deleted/', views.deleted_categories, name='deleted_categories'),
+    # path('<int:category_id>/restore/', views.category_restore, name='category_restore'),
 ]
--- category/views.py
+++ category/views.py
@@ -1,59 +1,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Category
+from django.views.generic import ListView,UpdateView,CreateView,DeleteView
+from django.urls import reverse_lazy
 
-def category_list(request):
-    categories = Category.getall()
-    return render(request, 'category/category_list.html', {'categories': categories})
-
-def category_new(request):
-    errormsg = None
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-
-        if not name:
-            errormsg = "Name is required."
-        else:
-            Category.Add(name=name, description=description)
-            return redirect('category:category_list')
-
-    return render(request, 'category/new.html', {'errormsg': errormsg})
-
-def category_update(request, category_id):
-    category = get_object_or_404(Category, id=category_id)
-    errormsg = None
-
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        description = request.POST.get('description')
-
-        if not name:
-            errormsg = "Name is required."
-        else:
-            Category.update(id=category.id, name=name, description=description)
-            return redirect('category:category_list')
-
-    return render(request, 'category/new.html', {'category': category, 'is_update': True, 'errormsg': errormsg})
-
-def category_soft_delete(request, category_id):
-    Category.softdelete(category_id)
-    return redirect('category:category_list')
-
-def category_hard_delete(request, category_id):
-    Category.harddelete(category_id)
-    return redirect('category:category_list')
-
-def deleted_categories(request):
-    deleted = Category.objects.filter(is_deleted=True)
-    return render(request, 'category/deleted_list.html', {'categories': deleted})
-
-def category_restore(request, category_id):
-    category = get_object_or_404(Category, id=category_id, is_deleted=True)
-    category.is_deleted = False
-    category.save()
-    return redirect('category:deleted_categories')
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'category/category_list.html'
+    context_object_name = 'categories'
 
 
+class CategoryCreateView(CreateView):
+    model = Category
+    fields = ['name', 'description']
+    template_name = 'category/new.html'
+    success_url = reverse_lazy('category:category_list')
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    fields = ['name', 'description']
+    template_name = 'category/category_form.html'
+    success_url = reverse_lazy('category:category_list')
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category/category_confirm_delete.html'
+    success_url = reverse_lazy('category:category_list')
 
 
 
